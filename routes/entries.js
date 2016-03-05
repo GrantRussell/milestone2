f93d5fda979f7aac072c98da952f12c9d3579f91
Grant Russell <grant.s.russ@gmail.com>
2016-03-04 18:55:01 -0700

    tried adding css and changed from blog to TIL

--- routes/entries.js
+++ routes/entries.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 var entries = [
-  {slug:"how to pass class", body: "come to class. do your homework", created_at: "some date"},
-  {slug:"how to fail class", body: "play video games all day", created_at: "some date"}
+  {slug:"TIL 03/02/2016", body: "Raccoons, while looking adorable, make horrible pets.", created_at: "03/02/2016"},
+  {slug:"TIL 03/04/2016", body: "Dogs love the taste of limes, even though they find them quite sour", created_at: "03/04/2016"}
 ];
 
 /* GET entris listing. */
 router.get('/', function(req, res, next) {
-  res.render('entries/index', { title: 'Blog', entries: entries });
+  res.render('entries/index', { title: 'TIL', entries: entries });
 });
 
 /* /entries/new */
@@ -21,7 +21,7 @@ router.post('/create', function(req, res, next) {
   console.log(req.body);
   entries.push(req.body);
   console.log(entries);
-  res.render('entries/index', { title: 'Blog', entries: entries });
+  res.render('entries/index', { title: 'TIL', entries: entries });
 });
 
 router.get('/update/:id', function(req, res, next) {
@@ -45,14 +45,14 @@ router.post('/update/:id', function(req, res, next) {
 
 /* GET entris listing. */
 router.get('/', function(req, res, next) {
-  res.render('entries/index_delete', { title: 'Blog', entries: entries });
+  res.render('entries/index_delete', { title: 'TIL', entries: entries });
 });
 
 
 
 /* /entries/0 */
 router.get('/:id', function(req, res, next) {
-  res.render('entries/entry', {title: "a entry", entry: entries[req.params.id]});
+  res.render('entries/entry', {title: "A TIL Entry", entry: entries[req.params.id]});
 });
 
 
